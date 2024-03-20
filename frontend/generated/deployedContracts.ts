854fc51f20b0b36270a5241eef6059f7f32d510e
Lucas Oliveira <olivmath@protonmail.com>
2024-03-20 12:22:12 -0300

    add: generate contracts

---
+++ frontend/generated/deployedContracts.ts
@@ -0,0 +1,3 @@
+const deployedContracts = {"31337": [{"name": "localhost", "chainId": "31337", "contracts": {"Counter": {"address": "0x5FbDB2315678afecb367f032d93F642f64180aa3", "abi": [{"type": "function", "name": "increment", "inputs": [], "outputs": [], "stateMutability": "nonpayable"}, {"type": "function", "name": "number", "inputs": [], "outputs": [{"name": "", "type": "uint256", "internalType": "uint256"}], "stateMutability": "view"}, {"type": "function", "name": "setNumber", "inputs": [{"name": "newNumber", "type": "uint256", "internalType": "uint256"}], "outputs": [], "stateMutability": "nonpayable"}]}}}]} as const; 
+
+export default deployedContracts
